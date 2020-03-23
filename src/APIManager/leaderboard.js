6057a2629fafc36c2f0b2106d9a28100bd72e4e4
geraldgsh <geraldgsh@hotmail.com>
2020-03-24 07:54:59 +0800

    Setup update leaderboard successful

--- src/APIManager/leaderboard.js
+++ src/APIManager/leaderboard.js
@@ -1,9 +1,22 @@
+/* eslint-disable quote-props */
 export default function updateLeaderboard() {
   const results = JSON.parse(localStorage.getItem('result'));
   const scoring = {
     user: results[0].user,
     score: results[0].score,
   };
-
   console.log(scoring);
+  console.log(JSON.stringify({ scoring }));
+  (async () => {
+    const rawResponse = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9uEL592fTdE2ZHjkFVdT/scores', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(scoring),
+    });
+    const content = await rawResponse.json();
+    console.log(content.result);
+  })();
 }
