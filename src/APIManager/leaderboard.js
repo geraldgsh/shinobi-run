9d8bb957aba24374ec5cdfebd8d366146a1173a7
geraldgsh <geraldgsh@hotmail.com>
2020-03-24 08:58:17 +0800

    setup start game after name input

--- src/APIManager/leaderboard.js
+++ src/APIManager/leaderboard.js
@@ -1,22 +1,25 @@
-/* eslint-disable quote-props */
+/* eslint-disable no-console, quote-props */
 export default function updateLeaderboard() {
   const results = JSON.parse(localStorage.getItem('result'));
-  const scoring = {
-    user: results[0].user,
-    score: results[0].score,
-  };
-  console.log(scoring);
-  console.log(JSON.stringify({ scoring }));
   (async () => {
-    const rawResponse = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9uEL592fTdE2ZHjkFVdT/scores', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(scoring),
-    });
-    const content = await rawResponse.json();
-    console.log(content.result);
+    const scoring = {
+      user: results[0].user,
+      score: results[0].score,
+    };
+    try {
+      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9uEL592fTdE2ZHjkFVdT/scores',
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(scoring),
+        });
+      const content = await response.json();
+      console.log(content.result);
+    } catch (err) {
+      throw new Error('Unable to fetch JSON response');
+    }
   })();
 }
--- src/assetManager/player.js
+++ src/assetManager/player.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-alert */
 import { Player, result } from '../constructor/constructor';
 
+const checkName = (name) => {
+  if (name === '') {
+    alert('Names can`t be blank!');
+    return false;
+  }
+  return true;
+};
+
 export default function newPlayer() {
+  const canvas = document.getElementById('gameCanvas');
   const results = JSON.parse(localStorage.getItem('result'));
   const player1Name = document.getElementById('player1name').value;
-
+  if (checkName(player1Name)) {
+    canvas.style.display = 'block';
+  }
   if (results === null) {
     const player1 = Player(player1Name, 0);
     result.push(player1);
