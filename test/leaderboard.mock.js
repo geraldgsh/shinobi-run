a2f50c5d29d0833330bdb0b9fa41f89fca6edf79
geraldgsh <geraldgsh@hotmail.com>
2020-03-26 02:35:45 +0800

    setup leaderboard testing

---
+++ test/leaderboard.mock.js
@@ -0,0 +1,62 @@
+async function getGameKey() {
+  try {
+    const rawResponse = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: 'Mock Test' }),
+      });
+    const content = await rawResponse.json();
+    return content;
+  } catch (err) {
+    throw new Error('Unable to fetch Test Game Key');
+  }
+}
+
+async function updateLeaderboard(name, points, key) {
+  try {
+    const rawResponse = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: name, score: points }),
+      });
+    const content = await rawResponse.json();
+    return content;
+  } catch (err) {
+    throw new Error('Unable to send score');
+  }
+}
+
+const sorter = (object) => {
+  const scoreArr = [];
+  for (let i = 0; i < object.length; i += 1) {
+    scoreArr.push([object[i].user, object[i].score]);
+  }
+  return Array.from(scoreArr).sort((a, b) => b[1] - a[1]);
+};
+
+async function getLeaderboard() {
+  try {
+    const scores = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+    const content = await scores.json();
+    return ranking(sorter(content.result));
+  } catch (err) {
+    throw new Error('Unable to fetch JSON response');
+  }
+}
+
+export { getGameKey, updateLeaderboard, getLeaderboard };
